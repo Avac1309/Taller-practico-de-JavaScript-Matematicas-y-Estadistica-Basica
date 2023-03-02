6603344bc7b6c18b76517828c20e1a4a48850e87
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-03-02 11:16:56 -0300

    Math en JavaScript

--- math.js
+++ math.js
@@ -1,40 +1,39 @@
-console.group("Cuadrado")
+console.group("Cuadrado") // incia 
+    const ladoCuadrado = 5;
+    const perimetroCuadrado = ladoCuadrado * 4;
+    const areaCuadrado = ladoCuadrado * ladoCuadrado + "cm²";
 
-const ladoCuadrado = 5;
-const perimetroCuadrado = ladoCuadrado * 4;
-const areaCuadrado = ladoCuadrado * ladoCuadrado + "cm²";
+    console.log({
+        ladoCuadrado,
+        perimetroCuadrado,
+        areaCuadrado,
+    })
+console.groupEnd("Cuadrado") // termina 
 
-console.log({
-    ladoCuadrado,
-    perimetroCuadrado,
-    areaCuadrado,
-})
 
-console.groupEnd("Cuadrado")
-console.group("Triangulo")
-
-function calcularPerimetroCuadrado(lado) {
-    return{
-        parametro: lado * 4,
-        area: lado * lado,
+console.group("Triangulo") // inicia
+    function calcularPerimetroCuadrado(lado) {
+        return{
+            parametro: lado * 4,
+            area: lado * lado,
+        }
     }
-}
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const ladoTrianguloBase = 4;
-const baseTriangulo = 4;
-const alturaTriangulo = 12; 
+    const ladoTriangulo1 = 6;
+    const ladoTriangulo2 = 6;
+    const ladoTrianguloBase = 4;
+    const baseTriangulo = 4;
+    const alturaTriangulo = 12; 
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2 ;
+    const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+    const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2 ;
 
-function calcularPerimetroTriangulo(lado1, lado2, base, altura) {
-    return{
-        parametro: lado1 + lado2 + base,
-        area: (base * altura) / 2
+    function calcularPerimetroTriangulo(lado1, lado2, base, altura) {
+        return{
+            parametro: lado1 + lado2 + base,
+            area: (base * altura) / 2
+        }
     }
-}
 
 console.log({
     ladoTriangulo1,
@@ -46,5 +45,33 @@ console.log({
     perimetroTriangulo,
     areaTriangulo,
 })
+console.groupEnd("Triangulo") // termina
 
-console.groupEnd("Triangulo")
+
+console.group("Circulo") // inica
+    const radioCirculo = 3;
+    const diametroCirculo = radioCirculo * 2;
+    const PI = 3.1415;
+
+    const circunferencia = diametroCirculo * PI;
+    const areaCirculo = (radioCirculo  ** 2) + PI;
+
+    console.log({
+        radioCirculo,
+        diametroCirculo,
+        PI,
+        circunferencia,
+        areaCirculo,
+    })
+
+    function calcularCirculo(radio) {
+        const diametro = radio * 2;
+        const radioAlCuadrado = Math.pow(radio, 2);
+
+        return{
+            circunferencia: diametro * Math.PI.toFixed(3), //toFixed(2) sirve para limitar los numeros y que no aparexcan tantos en ek calculo
+            area: radioAlCuadrado * Math.PI,
+        }
+    }
+
+console.groupEnd("Circulo") // termina 
